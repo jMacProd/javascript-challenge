ed59f9f32d878e68d427c956bf71ba4839cef70a
Jay <j.sutton@vic.chariot.net.au>
2021-03-12 16:04:40 +1100

    Appends full data to table in webpage.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,40 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Console.log the data from data.js
+//console.log(tableData);
+    //confirmed
+
+// Using the UFO dataset provided in the form of an array of
+    //JavaScript objects, write code that appends a table
+    //to your web page and then adds new rows of data for
+    // each UFO sighting.
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+
+//    tableData.forEach(function(sighting) {
+//    var row = tbody.append("tr");
+
+// Loop Through `data` to capture each object
+tableData.forEach(function(sighting) {
+    //console.log(sighting);
+        //confirmed
+    //Use d3 to append one table row `tr` for each weather report object
+    var row = tbody.append("tr");
+        //html in Elements confirmed
+    //Use `Object.entries` to collect each report value
+    Object.entries(sighting).forEach(function([key, value]) {
+    //console.log(key, value);
+            //confirmed
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+            //html in Elements confirmed
+        // Add the values to each cell
+        cell.text(value);
+            //confirm
+    });
+  });
+
+
