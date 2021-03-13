52320ad36e82ec0ed226c26d575b5c58c403b9bb
Jay <j.sutton@vic.chariot.net.au>
2021-03-13 16:35:40 +1100

    Backup app.py Part 2 table renders on opening web page

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,6 +1,31 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+
+//////////////////////////////////////
+// FULL DATA INITIAL LOAD
+
+function fulldata() {
+    tbody.html("");
+    console.log(`Complete data: ${tableData.length} records in this table.`);
+    console.log("----------");
+
+    tableData.forEach(function(sighting) {
+
+        var row = tbody.append("tr");
+
+        Object.entries(sighting).forEach(function([key, value]) {
+
+            var cell = row.append("td");
+
+            cell.text(value);
+        });
+    });
+}
+
+fulldata();
+
 /////////////////////////////////////
 // LISTENING EVENT
 
@@ -24,63 +49,70 @@ function runEnter() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
+    console.log('Filters:')
+    console.log('--------')
     //DATE
     // Select the input element and get the raw HTML node
     var inputElementdate = d3.select("#datetime");
     var inputValuedate = inputElementdate.property("value");
 
     //Added this conditional to allow blank values to return to main table
-    if (inputValuedate === "") {
-        console.log("No date entered");
-    }
-    else {
-        console.log(`The date entered is ${inputValuedate}`);
-    }
+    
+    //if (inputValuedate === "") {
+        //console.log("No date filter");
+    //}
+    //else {
+    //    console.log(`The date entered is ${inputValuedate}`);
+    //}
 
     //CITY
     // Select the input element and get the raw HTML node
     var inputElementcity = d3.select("#city");
     var inputValuecity = inputElementcity.property("value");
-    if (inputValuecity === "") {
-        console.log("No city entered");
-    }
-    else {
-        console.log(`The city entered is ${inputValuecity}`);
-    }
+    // if (inputValuecity === "") {
+    //     console.log("No city entered");
+    // }
+    // else {
+    //     console.log(`The city entered is ${inputValuecity}`);
+    // }
 
     //STATE
     // Select the input element and get the raw HTML node
     var inputElementstate = d3.select("#state");
     var inputValuestate = inputElementstate.property("value");
-    if (inputValuestate === "") {
-        console.log("No state entered");
-    }
-    else {
-        console.log(`The state entered is ${inputValuestate}`);
-    }
+    // if (inputValuestate === "") {
+    //     console.log("No state entered");
+    // }
+    // else {
+    //     console.log(`The state entered is ${inputValuestate}`);
+    // }
 
     //COUNTRY
     // Select the input element and get the raw HTML node
     var inputElementcountry = d3.select("#country");
     var inputValuecountry = inputElementcountry.property("value");
-    if (inputValuecountry === "") {
-        console.log("No country entered");
-    }
-    else {
-        console.log(`The country entered is ${inputValuecountry}`);
-    }
+    // if (inputValuecountry === "") {
+    //     console.log("No country entered");
+    // }
+    // else {
+    //     console.log(`The country entered is ${inputValuecountry}`);
+    // }
 
     //SHAPE
     // Select the input element and get the raw HTML node
     var inputElementshape = d3.select("#shape");
     var inputValueshape = inputElementshape.property("value");
-    if (inputValueshape === "") {
-        console.log("No shape entered");
-    }
-    else {
-        console.log(`The shape entered is ${inputValueshape}`);
-    }
-    
+    // if (inputValueshape === "") {
+    //     console.log("No shape entered");
+    // }
+    // else {
+    //     console.log(`The shape entered is ${inputValueshape}`);
+    // }
+    console.log(`Date: ${inputValuedate}`);
+    console.log(`City: ${inputValuecity}`);
+    console.log(`State: ${inputValuestate}`);
+    console.log(`Country: ${inputValuecountry}`);
+    console.log(`Shape: ${inputValueshape}`);
     //console.log("----------");
 
     //filteredtable(inputValuedate, inputValuecity, inputValuestate, inputValuecountry, inputValueshape)
@@ -138,15 +170,13 @@ function filteredtable(date, city, state, country, shape) {
 
 
     //This TableData should reflect each layer of filtering
-    console.log(tableDatashape);
-    console.log("-");
+    //console.log(tableDatashape);
+    //console.log("-");
 
     /////////////////////////////////////
 // ADD FILTERED DATA TO TABLE
 
     function addtable() {
-        // Get a reference to the table body
-        var tbody = d3.select("tbody");
         tbody.html("");
         console.log(`There are ${tableDatashape.length} records in this table.`);
         console.log("----------");
@@ -161,23 +191,6 @@ function filteredtable(date, city, state, country, shape) {
     addtable()
 }
 
-// /////////////////////////////////////
-// // ADD FILTERED DATA TO TABLE
-
-// function addtable() {
-//     // Get a reference to the table body
-//     var tbody = d3.select("tbody");
-//     tbody.html("");
-//     console.log(`There are ${tableDatashape.length} records in this table.`);
-//     console.log("----------");
-//     tableDatashape.forEach(function(sighting) {
-//         var row = tbody.append("tr");
-//         Object.entries(sighting).forEach(function([key, value]) {
-//             var cell = row.append("td");
-//             cell.text(value);
-//         });
-//     });
-// }
 
 
 
